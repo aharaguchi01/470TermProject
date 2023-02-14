4a47ae74efcb22f1f598857fd3ccf761e8c60c1f
ryangrogan <rgrogan@calpoly.edu>
2023-02-13 17:31:05 -0800

    Updated boids and hawk to flip better

--- boid.py
+++ boid.py
@@ -4,19 +4,19 @@ from vpython import *
 class boid:
     
     def __init__(self):
-        x, y, z = np.random.uniform(0,20), np.random.uniform(0,20), np.random.uniform(0,20)
-        a, b, c = np.random.uniform(-1,1), np.random.uniform(-1,1), np.random.uniform(-1,1)
-        self.cone = cone(pos=vector(x,y,z), axis=vector(a,b,c), length=1, radius=0.3, color=color.blue)
-        self.velocity = np.random.uniform(0,1)
+        x, y, z = np.random.uniform(0, 20), np.random.uniform(0, 20), np.random.uniform(0, 20)
+        a, b, c = np.random.uniform(-1, 1), np.random.uniform(-1, 1), np.random.uniform(-1, 1)
+        self.cone = cone(pos=vector(x, y, z), axis=vector(a, b, c), length=1, radius=0.3, color=color.blue)
+        self.velocity = np.random.uniform(0.5, 1.5)
         
     def move(self):
-        self.velocity = np.random.uniform(0,1)
+        self.velocity = np.random.uniform(0, 1)
         if self.cone.pos.x > 20 or self.cone.pos.x < 0:
-            self.cone.axis = -1 * self.cone.axis
-        elif self.cone.pos.y > 20 or self.cone.pos.y < 0:
-            self.cone.axis = -1 * self.cone.axis
-        elif self.cone.pos.z > 20 or self.cone.pos.z < 0:
-            self.cone.axis = -1 * self.cone.axis
+            self.cone.axis.x = -1 * self.cone.axis.x
+        if self.cone.pos.y > 20 or self.cone.pos.y < 0:
+            self.cone.axis.y = -1 * self.cone.axis.y
+        if self.cone.pos.z > 20 or self.cone.pos.z < 0:
+            self.cone.axis.z = -1 * self.cone.axis.z
         unit = self.cone.axis.norm()
         self.cone.pos = self.cone.pos + self.velocity * unit * 0.1
     
--- hawk.py
+++ hawk.py
@@ -8,17 +8,17 @@ class hawk:
         a, b, c = np.random.uniform(-1, 1), np.random.uniform(-1, 1), np.random.uniform(-1, 1)
 
         self.cone = cone(pos=vector(x, y, z), axis=vector(a, b, c), color=color.red)
-        self.velocity = np.random.uniform(0, 1)
+        self.velocity = np.random.uniform(0.5, 1)
 
-    def move(self, dt = 0.01):
+    def move(self, dt = 0.1):
         unit = self.cone.axis.norm()
         self.cone.pos = self.cone.pos + self.velocity*unit*dt
         if self.cone.pos.x > 20 or self.cone.pos.x < 0:
-            self.cone.axis = -self.cone.axis
+            self.cone.axis.x = -self.cone.axis.x
         if self.cone.pos.y > 20 or self.cone.pos.y < 0:
-            self.cone.axis = -self.cone.axis
+            self.cone.axis.y = -self.cone.axis.y
         if self.cone.pos.z > 20 or self.cone.pos.z < 0:
-            self.cone.axis = -self.cone.axis
+            self.cone.axis.z = -self.cone.axis.z
 
     def chase(self):
         pass
